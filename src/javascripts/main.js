abe353aa8aa17d63b650beb8167edcaf0e691769
Yitbarek Gitore <yitbareky@gmail.com>
2019-10-29 20:54:56 -0500

    added navbar

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,6 +1,11 @@
+import firebase from 'firebase';
 import '../styles/main.scss';
 
+import apiKeys from './helpers/apiKeys';
+
 const init = () => {
+  firebase.initializeApp(apiKeys.firebaseConfig);
+  console.log(firebase.apps);
   console.log('Hi');
 };
 
