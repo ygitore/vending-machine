5c5bcce632138e90d124326b395e639c5a3e0a0e
Yitbarek Gitore <yitbareky@gmail.com>
2019-11-02 10:33:55 -0500

    added seed json data and imported to firebase

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,12 +1,18 @@
 import firebase from 'firebase';
+
+import auth from './components/Auth/auth';
+import authData from './helpers/data/authData';
+import myNavbar from './components/myNavbar/myNavbar';
+
+import apiKeys from './helpers/apiKeys.json';
+
 import '../styles/main.scss';
 
-import apiKeys from './helpers/apiKeys';
-
 const init = () => {
-  firebase.initializeApp(apiKeys.firebaseConfig);
-  console.log(firebase.apps);
-  console.log('Hi');
+  firebase.initializeApp(apiKeys.firebaseKeys);
+  authData.checkLoginStatus();
+  auth.loginButton();
+  myNavbar.logoutEvent();
 };
 
 init();
