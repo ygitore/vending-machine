c768d429c4c8e38ea9a546122a6a542ab4bd61a3
Yitbarek Gitore <yitbareky@gmail.com>
2019-11-05 20:44:13 -0600

    finished purchase button function that reduces the number of items in stock

--- src/javascripts/helpers/data/snackData.js
+++ src/javascripts/helpers/data/snackData.js
@@ -19,4 +19,34 @@ const getSnacksByUid = (uid) => new Promise((resolve, reject) => {
 
 const addNewSnack = (newSnack) => axios.post(`${baseUrl}/snacks.json`, newSnack);
 
-export default { getSnacksByUid, addNewSnack };
+const updateSnack = (snackId, updatedSnack) => axios.put(`${baseUrl}/snacks/${snackId}.json`, updatedSnack);
+
+const buySnack = (snackId) => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/snacks/${snackId}.json`)
+    .then((result) => {
+      const snackObject = result.data;
+      snackObject.currentStocked = snackObject.currentStocked === 0 ? 0 : snackObject.currentStocked -= 1;
+      updateSnack(snackId, snackObject);
+      resolve();
+    })
+    .catch((error) => reject(error));
+});
+
+const restock = (snackId, restockNum) => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/snacks/${snackId}.json`)
+    .then((result) => {
+      const snackObject = result.data;
+      snackObject.currentStocked += restockNum;
+      snackObject.lifetimeNum += restockNum;
+      updateSnack(snackId, snackObject);
+      resolve();
+    })
+    .catch((error) => reject(error));
+});
+
+export default {
+  getSnacksByUid,
+  addNewSnack,
+  restock,
+  buySnack,
+};
